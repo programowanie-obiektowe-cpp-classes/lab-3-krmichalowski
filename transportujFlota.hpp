2afd20d7c757adc4a3b3d41c9fd6824524fa4fed
km_laptop <km>
2025-10-29 17:43:26 +0100

    wykonanie zadania

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,23 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    Stocznia stocznia{};
+    int transported = 0;
+    int ile_zagl = 0;
+
+    while(transported < towar)
+    {
+        Statek* s = stocznia();
+
+        Zaglowiec* z = dynamic_cast<Zaglowiec*>(s);
+        if(z != nullptr)
+        {
+            ile_zagl++;
+        }
+
+        transported += s->transportuj();
+        delete s;
+    }
+
+    return ile_zagl;
 }
